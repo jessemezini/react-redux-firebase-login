ccceafaf868e5b238d0f11d1bdfa76ec62e29cbd
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2016-10-11 17:18:35 -0300

    Initial config

---
+++ src/firebase/config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVEvORXF58Umn3GFkLWVi3OtLYW4szDGQ",
+  authDomain: "react-redux-firebase-login.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-login.firebaseio.com",
+  storageBucket: "react-redux-firebase-login.appspot.com",
+};
